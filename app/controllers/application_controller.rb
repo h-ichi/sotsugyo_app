c100a1636566aa7eb3ee4578e5657f5882144aa1
h-ichi <h.ichimiya.dosapo@gmail.com>
2025-08-25 15:09:25 +0900

    mvpver3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    suggestions_path
+    new_suggestion_path
   end
   
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
