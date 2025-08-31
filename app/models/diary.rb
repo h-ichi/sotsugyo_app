bf58c048ee57ec00b81b6355c64f59b26c114434
h-ichi <h.ichimiya.dosapo@gmail.com>
2025-08-31 21:59:49 +0900

    日記css調整

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,10 +1,7 @@
 class Diary < ApplicationRecord
-  class Diary < ApplicationRecord
     has_one_attached :image
   
     validates :title, presence: true
     validates :diary_date, presence: true
     validates :content, presence: true
-  end
-  
 end
