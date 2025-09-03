88b7074c008eb091743e86262d2dcb05f7325045
h-ichi <h.ichimiya.dosapo@gmail.com>
2025-09-03 09:06:30 +0900

    css調整

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,6 @@
 class DiariesController < ApplicationController
+  before_action :set_diary, only: [:show, :edit, :update, :destroy]
+
   def index
     @diaries = Diary.order(diary_date: :desc)
   end
@@ -20,7 +22,26 @@ class DiariesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @diary.update(diary_params)
+      redirect_to @diary, notice: "日記を更新しました。"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @diary.destroy
+    redirect_to diaries_path, notice: "日記を削除しました。"
+  end
+
   private
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
 
   def diary_params
     params.require(:diary).permit(:title, :diary_date, :content, :image)
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
 
 
-resources :diaries, only: [:new, :create, :index, :show]
+resources :diaries
 
 
 
